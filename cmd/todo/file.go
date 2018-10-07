831f32b1b6ee7a04192e96766b342ea01969b2be
Timo Reitz <godsboss@gmx.de>
2018-10-07 15:37:01 +0200

    Extract load() function.

--- cmd/todo/file.go
+++ cmd/todo/file.go
@@ -1,3 +1,19 @@
 package main
 
+import (
+	"github.com/GodsBoss/es-todo/es"
+	"github.com/GodsBoss/es-todo/es/json"
+
+	"os"
+)
+
 const filename = "events.json"
+
+func load() (*es.Events, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	return json.FromJSON(file)
+}
