762c3396ed3251feb99015a1c4778df29c8d2b16
Timo Reitz <godsboss@gmx.de>
2018-10-07 13:57:06 +0200

    Add task.

---
+++ todo/task.go
@@ -0,0 +1,43 @@
+package todo
+
+import (
+	"github.com/GodsBoss/es-todo/es"
+)
+
+type Task struct {
+	id          string
+	description string
+	state       string
+}
+
+func (task *Task) apply(event es.Event) {
+	switch event.Kind() {
+	case eventTaskAdded:
+		task.id = event.AggregateID()
+		task.state = taskStateOpen
+		task.description = string(event.Payload())
+	case eventTaskReworded:
+		task.description = string(event.Payload())
+	case eventTaskCanceled:
+		task.state = taskStateCanceled
+	case eventTaskFinished:
+		task.state = taskStateFinished
+	case eventTaskRemoved:
+		task.state = tasStatekRemoved
+	}
+}
+
+const (
+	taskStateOpen     = "open"
+	taskStateCanceled = "canceled"
+	taskStateFinished = "finished"
+	tasStatekRemoved  = "removed"
+)
+
+const (
+	eventTaskAdded    = "task_added"
+	eventTaskReworded = "task_reworded"
+	eventTaskCanceled = "task_canceled"
+	eventTaskFinished = "task_finished"
+	eventTaskRemoved  = "task_removed"
+)
